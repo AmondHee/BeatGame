abc27e07e20dd52d07a90814bc32fc31d123e980
kim <symem10@naver.com>
2017-09-09 14:12:38 +0900

    beat game tutorial / java

---
+++ Beat/src/kim/Beat1/DynamicBeat.java
@@ -0,0 +1,20 @@
+package kim.Beat1;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame{
+	public DynamicBeat(){
+		setTitle("Dynamic Beat!");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+
+}
---
+++ Beat/src/kim/Beat5/DynamicBeat.java
@@ -0,0 +1,100 @@
+package kim.Beat5;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class DynamicBeat extends JFrame{
+	private Image screenImage;
+	private Graphics screenGraphics;
+	
+	private Image introBackground = new ImageIcon(Main.class.getResource("../images/intro.jpg")).getImage();
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+	
+	private int mouseX, mouseY;
+	
+	private ImageIcon exitButtonEnteredImage = 
+			new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
+	private ImageIcon exitButtonBasicImage = 
+			new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+
+	
+	
+	public DynamicBeat(){
+		setUndecorated(true);
+		setTitle("Dynamic Beat!");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setBackground(new Color(0, 0, 0, 0));
+		setLayout(null);
+		
+		
+		exitButton.setBounds(1245,0,30,30);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		
+		exitButton.addMouseListener(new MouseAdapter() {
+			public void mouseEneterd(MouseEvent e){
+				exitButton.setIcon(exitButtonEnteredImage);
+			}
+			public void mouseExited(MouseEvent e){
+				exitButton.setIcon(exitButtonBasicImage);
+			}
+			public void mousePressed(MouseEvent e){
+				System.exit(0);
+			}
+			
+		});
+		
+		add(exitButton);
+		
+		
+		menuBar.setBounds(0,0,1280,30);
+		menuBar.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e){
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+			
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e){
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x-mouseX, y-mouseY);
+			}
+		});
+		add(menuBar);
+		
+		
+		Music introMusic = new Music("weAre.mp3", true);
+		introMusic.start();
+		
+	}
+
+	public void paint(Graphics g){
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphics = screenImage.getGraphics();
+		screeanDraw(screenGraphics);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screeanDraw(Graphics g){
+		g.drawImage(introBackground, 0, 0, null);
+		paintComponents(g);
+		this.repaint();
+	}
+}
